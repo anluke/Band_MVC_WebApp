0f4873f24b663866d207da9d887ead9e3dc9c697
Anton Lukenda <L00179664@atu.ie>
2024-05-19 08:24:29 +0100

    Add project files.

---
+++ BandWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BandWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
