0ce12d9893ac84801874ac3d5b699ab664a1fae6
Anton Lukenda <L00179664@atu.ie>
2024-05-21 19:51:35 +0100

    Adjusted store view and tour section. Fixed the landing page.

---
+++ BandWebApp/Areas/Customer/Controllers/BasicController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BandWebApp.Areas.Customer.Controllers
+{
+    public class BasicController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ BandWebApp/Areas/Customer/Controllers/StoreController.cs
@@ -0,0 +1,25 @@
+﻿using Band.DataAccess.Repository.IRepository;
+using Band.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BandWebApp.Areas.Customer.Controllers
+{
+    [Area("Customer")]
+    public class StoreController : Controller
+    {
+        private readonly ILogger<StoreController> _logger;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public StoreController(ILogger<StoreController> logger, IUnitOfWork unitOfWork)
+        {
+            _logger = logger;
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Product> productList = _unitOfWork.Product.GetAll(includeProperties: "Category");
+            return View(productList);
+        }
+    }
+}
